3c3dc547bd9f4322ff6eaa9af9450358067b3e70
sonalkawale3 <sonalkawale3@gmail.com>
2026-07-31 11:37:12 +0530

    backend bug fixed

--- env/dev/provider.tf
+++ env/dev/provider.tf
@@ -5,15 +5,15 @@ terraform{
             version = "4.1.0"
         }
     }
-}
+
 
 backend "azurerm" {
     resource_group_name  = "NetworkWatcherRG"
-    storage_account_name = "forasrpipeline "
+    storage_account_name = "forasrpipeline"
     container_name       = "virtualmachine"
     key                  = "dev.tfstate"
   }
-
+}
 provider "azurerm"{
     features{}
     subscription_id ="05dbb74f-6152-4a1d-a1fd-c49be5c3fd99"
