8372e0f2c467f96160dbe2645a3219ae943c433c
sonalkawale3 <sonalkawale3@gmail.com>
2026-07-31 10:58:15 +0530

    Add backend configuration for Azure storage

--- env/dev/provider.tf
+++ env/dev/provider.tf
@@ -6,6 +6,14 @@ terraform{
         }
     }
 }
+
+backend "azurerm" {
+    resource_group_name  = "NetworkWatcherRG"
+    storage_account_name = "forasrpipeline "
+    container_name       = "virtualmachine"
+    key                  = "dev.tfstate"
+  }
+
 provider "azurerm"{
     features{}
     subscription_id ="586e3c12-b0f0-4c52-946a-ed23650f6c3a"
